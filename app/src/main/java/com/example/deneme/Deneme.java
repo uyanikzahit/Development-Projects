bba9df97b4a3adcf0e2bd02424b106ab9bada87c
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-01-23 14:45:31 +0300

    test

---
+++ app/src/main/java/com/example/deneme/Deneme.java
@@ -0,0 +1,15 @@
+package com.example.deneme;
+
+public class Deneme {
+
+    public static void main(String args[]){
+
+
+        int sayi1 = 30;
+        sayi1++;
+        System.out.println(sayi1);
+
+
+    }
+
+}
