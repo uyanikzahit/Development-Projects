c9e263dafb0d71bbb3928fe41cfa9c46ddc798be
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-01-31 16:38:15 +0300

    Bottom Navigation

---
+++ ButtonNavigationKullanimi/app/src/main/java/com/example/buttonnavigationkullanimi/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.buttonnavigationkullanimi;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNav;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        bottomNav.findViewById(R.id.bottomNav);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        NavigationUI.setupWithNavController(bottomNav, navHostFragment.getNavController());
+
+    }
+}
