237090f8a52e80bff2060a3bd48a8e0ee7db92ca
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-02-14 16:50:24 +0300

    Design update

---
+++ KiilerUygulamas/app/src/main/java/com/example/kiileruygulamas/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.kiileruygulamas;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+    private Toolbar toolbar;
+    private RecyclerView rv;
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        rv = findViewById(R.id.rv);
+        fab = findViewById(R.id.fab);
+
+        toolbar.setTitle("Kişiler Uygulaması");
+        setSupportActionBar(toolbar);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+
+        MenuItem menuItem =menu.findItem(R.id.action_ara);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(this);
+
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Log.e("onQueryTextSubmit",query);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        Log.e("onQueryTextChange",newText);
+
+        return false;
+    }
+
+    public void alertGoster(){
+        LayoutInflater layout = LayoutInflater  .from(this);
+    }
+}
