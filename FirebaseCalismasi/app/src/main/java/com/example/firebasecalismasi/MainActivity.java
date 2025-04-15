f1726bbcc0046962139df81c5582ab9b587ebb68
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-04-15 20:10:17 +0300

    firebase repo

---
+++ FirebaseCalismasi/app/src/main/java/com/example/firebasecalismasi/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.firebasecalismasi;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+}
