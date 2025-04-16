8d07d82d941f28f939791d15e20e6e6d999fe589
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-04-16 14:22:35 +0300

    Firebase extra installation

--- FirebaseCalismasi/build.gradle.kts
+++ FirebaseCalismasi/build.gradle.kts
@@ -1,4 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript{
+    dependencies{
+        classpath("androidx.navigation.safeargs:androidx.navigation.safeargs.gradle.plugin:2.8.9")
+    }
+}
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.google.gms.google.services) apply false
