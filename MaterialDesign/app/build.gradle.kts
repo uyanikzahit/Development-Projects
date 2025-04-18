7d3738d46df2cf010a2a89405d26479a7f017431
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-04-18 17:39:45 +0300

    Firebase extra installation

--- MaterialDesign/app/build.gradle.kts
+++ MaterialDesign/app/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
+
+
+// Firebase ekstra kurulum linki: https://firebase.google.com/docs/android/setup#available-libraries
 android {
     namespace = "com.example.materialdesign"
     compileSdk = 35
@@ -25,6 +29,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -32,12 +37,19 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+
+
+
+    implementation("com.google.firebase:firebase-database:21.0.0")
+
 }
