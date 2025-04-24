711632e1134316eefb07f26974d81198cb998f91
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-04-24 17:50:12 +0300

    permission control

--- KonumKullanimi/app/src/main/java/com/example/konumkullanimi/MainActivity.java
+++ KonumKullanimi/app/src/main/java/com/example/konumkullanimi/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.konumkullanimi;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
 
 import com.example.konumkullanimi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding tasarim;
+    private int izinKontrol= 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +29,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(tasarim.getRoot());
 
         tasarim.buttonKonumAl.setOnClickListener(view ->{
+            izinKontrol = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
 
+            if(izinKontrol != PackageManager.PERMISSION_GRANTED){//İzin onaylanmamışsa.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},100);
+
+            }else{//İzin onaylanmışsa.
+
+            }
         });
 
+    }
+
+    @Override
+    @SuppressLint("MissingSuperCall")
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == 100){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getApplicationContext(),"İzin kabul edildi.",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getApplicationContext(),"İzin reddedildi",Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
     }
 }
