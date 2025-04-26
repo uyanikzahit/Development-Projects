e29456b82409df8b57be56f6e656b8229b6b067e
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-04-26 21:20:22 +0300

    Using Event Handle

--- MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MainActivity.java
+++ MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MainActivity.java
@@ -12,39 +12,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         tasarim = ActivityMainBinding.inflate(getLayoutInflater());
+        tasarim.setMainActivityNesnesi(this);
         setContentView(tasarim.getRoot());
 
         tasarim.textViewSonuc.setText("0");
 
-        tasarim.buttonToplama.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String alinanSayi1 = tasarim.editTextSayi1.getText().toString();
-                String alinanSayi2 = tasarim.editTextSayi2.getText().toString();
+    }
 
-                int sayi1 = Integer.parseInt(alinanSayi1);
-                int sayi2 = Integer.parseInt(alinanSayi2);
+    public void buttonToplamaTikla(String alinanSayi1,String alinanSayi2){
 
-                int toplam = sayi1 + sayi2;
 
-                tasarim.textViewSonuc.setText(String.valueOf(toplam));
-            }
-        });
+        // xml tarafında bu kodlamaları yaptığımız için bu bölüme gerek kalmadı
+//        String alinanSayi1 = tasarim.editTextSayi1.getText().toString();
+//        String alinanSayi2 = tasarim.editTextSayi2.getText().toString();
 
-        tasarim.buttonCarpma.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String alinanSayi1 = tasarim.editTextSayi1.getText().toString();
-                String alinanSayi2 = tasarim.editTextSayi2.getText().toString();
+        int sayi1 = Integer.parseInt(alinanSayi1);
+        int sayi2 = Integer.parseInt(alinanSayi2);
 
-                int sayi1 = Integer.parseInt(alinanSayi1);
-                int sayi2 = Integer.parseInt(alinanSayi2);
+        int toplam = sayi1 + sayi2;
 
-                int carpma = sayi1 * sayi2;
+        tasarim.textViewSonuc.setText(String.valueOf(toplam));
 
-                tasarim.textViewSonuc.setText(String.valueOf(carpma));
-            }
-        });
+    }
+
+    public void buttonCarpmaTikla(String alinanSayi1,String alinanSayi2){
+
+
+        // xml tarafında bu kodlamaları yaptığımız için bu bölüme gerek kalmadı
+//        String alinanSayi1 = tasarim.editTextSayi1.getText().toString();
+//        String alinanSayi2 = tasarim.editTextSayi2.getText().toString();
+
+        int sayi1 = Integer.parseInt(alinanSayi1);
+        int sayi2 = Integer.parseInt(alinanSayi2);
+
+        int carpma = sayi1 * sayi2;
+
+        tasarim.textViewSonuc.setText(String.valueOf(carpma));
 
     }
 }
