e8b4346a202cc16bd38646fbd43048ee6add3563
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-04-28 13:24:13 +0300

    ViewModel usage

--- MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MainActivity.java
+++ MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MainActivity.java
@@ -3,21 +3,25 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.example.mvvmkullanimi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding tasarim;
+    private MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         tasarim = ActivityMainBinding.inflate(getLayoutInflater());
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
         tasarim.setMainActivityNesnesi(this);
         setContentView(tasarim.getRoot());
 
 //        tasarim.textViewSonuc.setText("0");
 
-        tasarim.setHesaplamaSonucu("0");
+        tasarim.setHesaplamaSonucu(viewModel.getSonuc());
 
     }
 
@@ -28,12 +32,16 @@ public class MainActivity extends AppCompatActivity {
 //        String alinanSayi1 = tasarim.editTextSayi1.getText().toString();
 //        String alinanSayi2 = tasarim.editTextSayi2.getText().toString();
 
-        int sayi1 = Integer.parseInt(alinanSayi1);
-        int sayi2 = Integer.parseInt(alinanSayi2);
+//        int sayi1 = Integer.parseInt(alinanSayi1);
+//        int sayi2 = Integer.parseInt(alinanSayi2);
+//
+//        int toplam = sayi1 + sayi2;
 
-        int toplam = sayi1 + sayi2;
 
-        tasarim.setHesaplamaSonucu(String.valueOf(toplam));
+
+        viewModel.toplamaYap(alinanSayi1,alinanSayi2);
+
+        tasarim.setHesaplamaSonucu(viewModel.getSonuc());
 
 
     }
@@ -45,14 +53,13 @@ public class MainActivity extends AppCompatActivity {
 //        String alinanSayi1 = tasarim.editTextSayi1.getText().toString();
 //        String alinanSayi2 = tasarim.editTextSayi2.getText().toString();
 
-        int sayi1 = Integer.parseInt(alinanSayi1);
-        int sayi2 = Integer.parseInt(alinanSayi2);
+//        int sayi1 = Integer.parseInt(alinanSayi1);
+//        int sayi2 = Integer.parseInt(alinanSayi2);
+//
+//        int carpma = sayi1 * sayi2;
+        viewModel.carpmaYap(alinanSayi1,alinanSayi2);
 
-        int carpma = sayi1 * sayi2;
-
-        tasarim.textViewSonuc.setText(String.valueOf(carpma));
-
-        tasarim.setHesaplamaSonucu(String.valueOf(carpma));
+        tasarim.setHesaplamaSonucu(viewModel.getSonuc());
 
 
     }
---
+++ MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MainActivityViewModel.java
@@ -0,0 +1,41 @@
+package com.example.mvvmkullanimi;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+    private String sonuc = "0";
+
+    public String getSonuc() {
+        return sonuc;
+    }
+
+    public void toplamaYap(String alinanSayi1,String alinanSayi2){
+
+
+
+
+        int sayi1 = Integer.parseInt(alinanSayi1);
+        int sayi2 = Integer.parseInt(alinanSayi2);
+
+        int toplam = sayi1 + sayi2;
+
+        sonuc = String.valueOf(toplam);
+
+
+
+    }
+
+    public void carpmaYap(String alinanSayi1,String alinanSayi2){
+
+
+
+        int sayi1 = Integer.parseInt(alinanSayi1);
+        int sayi2 = Integer.parseInt(alinanSayi2);
+
+        int carpma = sayi1 * sayi2;
+
+        sonuc = String.valueOf(carpma);
+
+
+    }
+}
