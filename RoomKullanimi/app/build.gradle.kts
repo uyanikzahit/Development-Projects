36f57d085e7eaa605510af1beb9b2683448161cb
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-04-30 19:59:07 +0300

    Room setup

---
+++ RoomKullanimi/app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.roomkullanimi"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.roomkullanimi"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    //Room
+    testImplementation(libs.room.rxjava2)
+    testImplementation(libs.room.compiler)
+    //RxJava
+    testImplementation(libs.room.rxjava2)
+    testImplementation(libs.rxandroid)
+
+
+
+
+}
