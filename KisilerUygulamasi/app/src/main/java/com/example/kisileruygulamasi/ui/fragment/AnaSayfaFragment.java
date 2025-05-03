655eda20e45d7fa5de4d4dadf639b35c19e53a12
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-05-03 18:46:36 +0300

    contacts app update

--- KisilerUygulamasi/app/src/main/java/com/example/kisileruygulamasi/ui/fragment/AnaSayfaFragment.java
+++ KisilerUygulamasi/app/src/main/java/com/example/kisileruygulamasi/ui/fragment/AnaSayfaFragment.java
@@ -1,19 +1,30 @@
 package com.example.kisileruygulamasi.ui.fragment;
 
+import android.app.admin.ManagedSubscriptionsPolicy;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
 import androidx.navigation.Navigation;
 
 import com.example.kisileruygulamasi.databinding.FragmentAnaSayfaBinding;
+
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.kisileruygulamasi.R;
 
 
-public class AnaSayfaFragment extends Fragment {
+public class AnaSayfaFragment extends Fragment implements SearchView.OnQueryTextListener{
 
     private FragmentAnaSayfaBinding tasarim;
     @Override
@@ -22,9 +33,42 @@ public class AnaSayfaFragment extends Fragment {
         tasarim =   FragmentAnaSayfaBinding.inflate(inflater, container, false);
 
         tasarim.toolbarAnasayfa.setTitle("Kişiler");
+        ((AppCompatActivity)getActivity()).setSupportActionBar(tasarim.toolbarAnasayfa);
         tasarim.fab.setOnClickListener(view->{
             Navigation.findNavController(view).navigate(R.id.kisiKayitGecis);
         });
+
+        requireActivity().addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.toolbar_menu,menu);
+
+                MenuItem item = menu.findItem(R.id.action_ara);
+                SearchView searchView = (SearchView) item.getActionView();
+                searchView.setOnQueryTextListener(AnaSayfaFragment.this);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        },getViewLifecycleOwner(), Lifecycle.State.RESUMED);
         return tasarim.getRoot();
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        ara(query);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        ara(newText);
+        return true;
+    }
+
+    public void ara(String aramaKelimesi){
+        Log.e("Kişi Ara", aramaKelimesi);
     }
 }
--- KisilerUygulamasi/app/src/main/java/com/example/kisileruygulamasi/ui/fragment/KisiDetayFragment.java
+++ KisilerUygulamasi/app/src/main/java/com/example/kisileruygulamasi/ui/fragment/KisiDetayFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import com.example.kisileruygulamasi.databinding.FragmentKisiDetayBinding;
+import com.example.kisileruygulamasi.data.entity.Kisiler;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +22,26 @@ public class KisiDetayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         tasarim = FragmentKisiDetayBinding.inflate(inflater, container, false);
+
+        tasarim.toolbarKisiDetay.setTitle("Kişi Detay");
+
+        KisiDetayFragmentArgs bundle = KisiDetayFragmentArgs.fromBundle(getArguments());
+        Kisiler gelenKisi = bundle.getKisi();
+
+        tasarim.editTextKisiAd.setText(gelenKisi.getKisi_ad());
+        tasarim.editTextKisiTel.setText(gelenKisi.getKisi_tel());
+
+        tasarim.buttonGuncelle.setOnClickListener(view -> {
+            String kisi_ad = tasarim.editTextKisiAd.getText().toString();
+            String kisi_tel = tasarim.editTextKisiTel.getText().toString();
+
+            guncelle(gelenKisi.getKisi_id(), gelenKisi.getKisi_ad(), gelenKisi.getKisi_tel());
+        });
+
         return tasarim.getRoot();
+    }
+
+    public void guncelle(int kisi_id, String kisi_ad, String kisi_tel){
+        Log.e("Kişi Güncelle",kisi_id +" - "+ kisi_ad+" - "+kisi_tel);
     }
 }
