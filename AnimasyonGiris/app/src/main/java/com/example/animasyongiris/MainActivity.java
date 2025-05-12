62f6c1b92b07e345bf5b4f1a7e60e69063819edb
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-05-12 16:13:47 +0300

    Anim app 1

--- AnimasyonGiris/app/src/main/java/com/example/animasyongiris/MainActivity.java
+++ AnimasyonGiris/app/src/main/java/com/example/animasyongiris/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.animasyongiris;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
+    private Animation animasyon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        button = findViewById(R.id.button);
+
+        animasyon = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animgiris);
+//        button.startAnimation(animasyon);
+        button.setAnimation(animasyon);
+
     }
 }
