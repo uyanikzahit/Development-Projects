6e32fff3a43af91546e4157c7cb0ac09da71c6a8
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-05-13 11:39:28 +0300

    Animasyon Scale - 2

--- AnimStudy/app/src/main/java/com/example/animstudy/MainActivity.java
+++ AnimStudy/app/src/main/java/com/example/animstudy/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
