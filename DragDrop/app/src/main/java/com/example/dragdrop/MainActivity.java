0e8784426df13aeeea58a720622828f100ba6b80
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-05-15 12:40:48 +0300

    drag & drop app update - 2

--- DragDrop/app/src/main/java/com/example/dragdrop/MainActivity.java
+++ DragDrop/app/src/main/java/com/example/dragdrop/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.dragdrop;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +16,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonKopyala, buttonGit;
+    private EditText editText;
+
+    private ClipboardManager kopyalamaPanom;
+    private ClipData clipData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +29,35 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        buttonKopyala = findViewById(R.id.buttonKopyala);
+        buttonGit = findViewById(R.id.buttonGit);
+        editText = findViewById(R.id.editText);
+
+        kopyalamaPanom = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        buttonKopyala.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String kopyalanacakYazi  = editText.getText().toString();
+                clipData = ClipData.newPlainText("text",kopyalanacakYazi);
+
+                kopyalamaPanom.setPrimaryClip(clipData);
+
+                Toast.makeText(getApplicationContext(), "Panoya Kopyalandır",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        buttonGit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this,SecondActivity.class));
+
+            }
+        });
 
     }
 }
--- DragDrop/app/src/main/java/com/example/dragdrop/SecondActivity.java
+++ DragDrop/app/src/main/java/com/example/dragdrop/SecondActivity.java
@@ -1,6 +1,13 @@
 package com.example.dragdrop;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +17,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private ClipboardManager kopyalamaPanom;
+    private ClipData clipData;
+    private Button buttonYapistir;
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        buttonYapistir = findViewById(R.id.buttonYapistir);
+        textView = findViewById(R.id.textView);
+
+
+        kopyalamaPanom = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        buttonYapistir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                clipData = kopyalamaPanom.getPrimaryClip();
+                ClipData.Item item = clipData.getItemAt(0);
+                String kopyalanmisYazi = item.getText().toString();
+
+                textView.setText(kopyalanmisYazi);
+
+                Toast.makeText(getApplicationContext(), "Yazı Yapıştırıldı",Toast.LENGTH_SHORT).show();
+
+            }
         });
+
+
     }
 }
