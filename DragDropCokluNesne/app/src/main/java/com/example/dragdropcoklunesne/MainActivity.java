8c90dc800e20b708ce1295f83161ac0ef7c8116a
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-05-16 11:42:39 +0300

    DragDrop multiple processes coding

--- DragDropCokluNesne/app/src/main/java/com/example/dragdropcoklunesne/MainActivity.java
+++ DragDropCokluNesne/app/src/main/java/com/example/dragdropcoklunesne/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.dragdropcoklunesne;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -58,12 +63,61 @@ public class MainActivity extends AppCompatActivity implements View.OnDragListen
     }
 
     @Override
-    public boolean onDrag(View v, DragEvent event) {
+    public boolean onDrag(View view, DragEvent dragEvent) {
+
+        switch (dragEvent.getAction()){
+            case DragEvent.ACTION_DRAG_STARTED:
+                return true;
+            case DragEvent.ACTION_DRAG_ENTERED:
+                view.getBackground().setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_IN);
+                view.invalidate();
+                return true;
+            case DragEvent.ACTION_DRAG_LOCATION:
+                return true;
+            case DragEvent.ACTION_DRAG_EXITED:
+                view.getBackground().clearColorFilter();
+                view.invalidate();
+                return true;
+            case DragEvent.ACTION_DROP:
+                view.getBackground().clearColorFilter();
+                view.invalidate();
+
+                View gorselNesne = (View) dragEvent.getLocalState();
+
+                ViewGroup eskiTasarimAlani = (ViewGroup) gorselNesne.getParent();
+
+                eskiTasarimAlani.removeView(gorselNesne);
+
+                LinearLayout hedefTasarimAlani = (LinearLayout) view;
+
+                hedefTasarimAlani.addView(gorselNesne);
+
+                gorselNesne.setVisibility(View.VISIBLE);
+
+                return true;
+            case DragEvent.ACTION_DRAG_ENDED:
+                view.getBackground().clearColorFilter();
+                view.invalidate();
+                return true;
+            default:break;
+        }
+
         return false;
     }
 
     @Override
-    public boolean onLongClick(View v) {
-        return false;
+    public boolean onLongClick(View view) {
+
+        ClipData.Item item = new ClipData.Item((CharSequence) view.getTag());
+
+        String [] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+
+        ClipData clipData = new ClipData(view.getTag().toString(),mimeTypes,item);
+
+        View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+        view.startDrag(clipData,shadowBuilder,view,0);
+
+        view.setVisibility(View.INVISIBLE);
+        return true;
     }
 }
