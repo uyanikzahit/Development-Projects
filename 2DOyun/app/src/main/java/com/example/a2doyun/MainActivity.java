7fae029ad3c11f0c01e2e8a02ccb1746aa29c325
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-05-20 10:39:14 +0300

    Introduction and design

---
+++ 2DOyun/app/src/main/java/com/example/a2doyun/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.a2doyun;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button buttonBasla;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        buttonBasla = findViewById(R.id.buttonBasla);
+
+        buttonBasla.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return false;
+            }
+        });
+    }
+}
